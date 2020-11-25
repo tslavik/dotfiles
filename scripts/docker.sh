1dee1a40cb2e444a282f7293c52c016de4cb63ad
Tomas Slavik <tomas.slavik@microsoft.com>
2020-11-25 16:30:50 +0100

    cli is enough

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -64,7 +64,7 @@ else
     curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | (OUT=$(apt-key add - 2>&1) || echo $OUT)
     echo "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
     apt-get update
-    apt-get -y install --no-install-recommends docker-ce docker-ce-cli containerd.io
+    apt-get -y install --no-install-recommends docker-ce-cli
 fi
 
 # Install Docker Compose if not already installed 
