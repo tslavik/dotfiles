7bd7a95b5cb0bcb9d2d02de2b4e007a5db6578d3
Tomas Slavik <tomas.slavik@microsoft.com>
2020-12-23 15:54:45 +0100

    script docker modified

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
@@ -24,13 +24,13 @@ fi
 # Determine the appropriate non-root user
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
-    # POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    # for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
-    #     if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-    #         USERNAME=${CURRENT_USER}
-    #         break
-    #     fi
-    # done
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
     if [ "${USERNAME}" = "" ]; then
         USERNAME=root
     fi
